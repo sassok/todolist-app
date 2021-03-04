c256ef43d6d37570f18a07f8105eed41d7304bbe
sassok <kevin.sassoo@gmail.com>
2021-03-04 16:13:31 +0100

    Ajac start

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,6 @@
 class EmailsController < ApplicationController
   def index
+    @emails = Email.all
   end
 
   def new
@@ -7,8 +8,18 @@ class EmailsController < ApplicationController
   end
 
   def create
-    @email = Email.all
-    Email.create!(object: Faker::Lorem.word, body: Faker::Lorem.paragraph, status: false)
+   @email = Email.create!(object: Faker::Lorem.word, body: Faker::Lorem.paragraph)
+    respond_to do |format|
+      if @email.save
+          format.html { redirect_to root_path }
+          format.js { }
+        flash[:notice] = "Task created"
+      else
+        format.html { redirect_to root_path }
+        format.js { }
+        flash[:notice] = "Please try again"
+      end
+    end
   end
 
 end
