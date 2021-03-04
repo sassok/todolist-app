55438f37fbd639139f392c62e48f554cd9583092
Neeiyo <louis.grandjean99@gmail.com>
2021-03-04 12:29:26 +0100

    button new

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,14 @@
 class EmailsController < ApplicationController
   def index
   end
+
+  def new
+    @email = Email.new
+  end
+
+  def create
+    @email = Email.all
+    Email.create!(object: Faker::Lorem.word, body: Faker::Lorem.paragraph, status: false)
+  end
+
 end
